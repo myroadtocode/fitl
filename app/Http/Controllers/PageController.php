e6d500af58d6ac2d21a952aa13f2449a86dfbc91
“Milan <myroadtocode@gmail.com>
2016-02-23 07:15:11 +0100

    Initial fitl application

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller{
+
+	public function about(){
+		return view('pages/about');
+	}
+	
+}
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<title>About</title>
+	</head>
+	<body>
+		<h1>About</h1>
+		<p>This is the application I'm building in the <a href="http://selftaughtcoders.com/from-idea-to-launch">From Idea To Launch</a>
+		course</p>
+		<p>This web application will allow people to boyh submit and aswer questions for various programming languages.</p>
+	</body>
+</html>
